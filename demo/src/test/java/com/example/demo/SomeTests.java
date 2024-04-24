8e60472489af4bc1340ef243bc54d62657e9d2ef
anthony <47711333+Anthony-Jhoiro@users.noreply.github.com>
2024-04-24 21:02:59 +0200

    :tada: Add demo project

---
+++ demo/src/test/java/com/example/demo/SomeTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeTests {
+
+  @Test
+  public void test1() {
+    assertTrue(true);
+  }
+}
